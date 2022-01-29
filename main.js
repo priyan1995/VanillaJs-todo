7a9d881ecc9bb725045be861f884d8226326985d
priyan <45042030+priyan1995@users.noreply.github.com>
2022-01-29 19:44:35 +0530

    items append to the inner html

--- main.js
+++ main.js
@@ -7,5 +7,29 @@ todoBtn.addEventListener('click',addTodo);
 
 function addTodo(e){
     e.preventDefault();
-    console.log("todo-added");   
+
+    // todo div creation
+    const todoDIv = document.createElement("div");
+    todoDIv.classList.add("todo");
+
+    // create li
+    const newTodo = document.createElement("li");
+    newTodo.innerText = "test";
+    newTodo.classList.add("todo-item");
+    todoDIv.appendChild(newTodo);
+
+    // create Complete Button
+    const completeBtn = document.createElement("button");
+    completeBtn.innerHTML = "Done";
+    completeBtn.classList.add("btn","btn-success");
+    todoDIv.appendChild(completeBtn);
+
+    // create Delete Button
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "Delete";
+    deleteBtn.classList.add("btn","btn-danger");
+    todoDIv.appendChild(deleteBtn);
+
+    // append to list
+    todoList.appendChild(todoDIv);
 }
